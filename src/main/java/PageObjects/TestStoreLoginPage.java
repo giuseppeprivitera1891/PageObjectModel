dfa9bbe4e001e58646978d930e23276010d89276
giuse <giuse@DESKTOP-DHD9Q3R.wind3.hub>
2023-06-02 21:49:08 +0200

    Selectors elements

--- src/main/java/PageObjects/TestStoreLoginPage.java
+++ src/main/java/PageObjects/TestStoreLoginPage.java
@@ -1,9 +1,16 @@
 package PageObjects;
 
-public class TestStoreLoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-	public TestStoreLoginPage() {
-		
+public class TestStoreLoginPage {
+	WebDriver driver;
+
+	public TestStoreLoginPage(WebDriver driver) {
+		this.driver = driver;
 	}
 	
+	By email = By.cssSelector("section input[name='email']");
+	By password = By.cssSelector("input[name='password']");
+	By loginBtn = By.cssSelector("button#submit-login");
 }
