0839df46035b82b03fb91b4093e7cf671f2d43bd
giuse <giuse@DESKTOP-DHD9Q3R.wind3.hub>
2023-06-02 22:00:02 +0200

    Get web elements

--- src/main/java/PageObjects/TestStoreLoginPage.java
+++ src/main/java/PageObjects/TestStoreLoginPage.java
@@ -2,6 +2,7 @@ package PageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class TestStoreLoginPage {
 	WebDriver driver;
@@ -13,4 +14,16 @@ public class TestStoreLoginPage {
 	By email = By.cssSelector("section input[name='email']");
 	By password = By.cssSelector("input[name='password']");
 	By loginBtn = By.cssSelector("button#submit-login");
+	
+	public WebElement getEmail() {
+		return driver.findElement(email);
+	}
+	
+	public WebElement getPassword() {
+		return driver.findElement(password);
+	}
+	
+	public WebElement getLoginBtn() {
+		return driver.findElement(loginBtn);
+	}
 }
